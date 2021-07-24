5ca380320c10a1d157713c79bdd14f528f403d3d
Aquiles Aguiar <aquilesvibe@gmail.com>
2021-07-24 18:44:22 -0300

    Testes

--- app.py
+++ app.py
@@ -1,27 +1,24 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import requests
+from bs4 import BeautifulSoup
 from time import sleep
 
 driver = webdriver.Firefox()
-driver.get("https://auth.uber.com/login/?breeze_local_zone=dca1&next_url=https%3A%2F%2Fm.uber.com%2F%3Fuclick_id%3D6db58b11-59e5-4b63-b64e-00de7d0e0e04&state=Dp6iqEL9JlSX5h-p8K6LcCb5wElGfILD7eq8pJyL3kM%3D")
-
-campoNum = driver.find_element_by_name("phoneNumber")
-campoNum.send_keys('27988125213')
-campoNum.send_keys(Keys.ENTER)
-
-try:
-    codCel = str(input('Insira o código de celular >>'))
-    driver.find_element_by_xpath('//*[@id="smsOTP"]').send_keys(codCel)
-    driver.find_element_by_xpath('//*[@id="smsOTP"]').send_keys(Keys.ENTER)
-except:
-    pass
-
+driver.get("https://auth.uber.com/login/session")
+email = driver.find_element_by_id("useridInput")
+email.send_keys("aquilesvibe@hotmail.com")
+email.send_keys(Keys.ENTER)
 sleep(10)
-driver.find_element_by_xpath('//*[@id="password"]').send_keys('7412369')
-driver.find_element_by_xpath('//*[@id="password"]').send_keys(Keys.ENTER)
+driver.find_element_by_id('password').send_keys('7412369')
+driver.find_element_by_id('password').send_keys(Keys.ENTER)
+
 
 sleep(10)
 driver.get("https://riders.uber.com/trips")
-r = requests.get(driver.get("https://riders.uber.com/trips"))
-print(r.text)
+# html = requests.get(driver.current_url)
+# print(html.text)
+r = BeautifulSoup(driver.page_source, 'html.parser')
+print(r.prettify())
+with open('pagina.txt','w') as pagina:
+    pagina.write(str(r.prettify()))
